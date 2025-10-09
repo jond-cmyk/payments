63d4f03f376075f55f5175590d9092c8f8cf806f
[dyad] <jon.d@kassoehousing.com>
2025-10-09 11:23:50 +0100

    [dyad] Fixing main.tsx to render the App component - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'; // Import the main App component
 import './globals.css'; // Keep global styles
 
 console.log("main.tsx is executing!"); // Added for debugging
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="p-8 text-center text-2xl font-bold">
-      Hello World from main.tsx!
-    </div>
+    <App /> {/* Render the App component */}
   </React.StrictMode>,
 );
