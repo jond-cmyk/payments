15d1fc03b3e4479b84d27860f7f300c79abf8702
[dyad] <jon.d@kassoehousing.com>
2025-10-09 11:05:51 +0100

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,1 @@
-debugger; // Added this line to force a pause in browser execution
-
-console.log("main.tsx: Script file loaded and starting execution.");
-
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
-
-console.log("main.tsx script started.");
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(<App />);
-  console.log("React app render initiated.");
-} else {
-  console.error("Root element with ID 'root' not found. React app cannot be mounted.");
-}
+console.log("Hello from main.tsx - testing execution!");
