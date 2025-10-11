eb898d87f85f955615651b4f1ae5989db4fe97ba
[dyad] <jon.d@kassoehousing.com>
2025-10-11 13:39:19 +0100

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globals.css';
-import ErrorBoundary from './components/ErrorBoundary'; // Import ErrorBoundary
+import ErrorBoundary from './components/ErrorBoundary';
 
-console.log("main.tsx is executing!"); // Added for debugging
+console.log("main.tsx is executing!");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary> {/* Wrap App with ErrorBoundary */}
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
+  // Temporarily removed React.StrictMode to debug potential double-rendering issues
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
 );
