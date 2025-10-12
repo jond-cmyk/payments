39aabf7243328e9f7cf7f7d303c1ca4807a65572
[dyad] <jon.d@kassoehousing.com>
2025-10-12 10:19:53 +0100

    [dyad] wrote 2 file(s)

--- src/pages/PaymentRequestDetail.tsx
+++ src/pages/PaymentRequestDetail.tsx
@@ -474,7 +474,7 @@ const PaymentRequestDetail = () => {
         setIsEditing={setIsEditing}
         editForm={editForm}
         handleRequesterEditSubmit={handleRequesterEditSubmit}
-        auditUsers={auditUsers} {/* Pass auditUsers here */}
+        auditUsers={auditUsers}
       />
 
       <AdminActionsCard
