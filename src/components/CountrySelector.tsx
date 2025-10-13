7512ed0746f97df9cef20c0dea848479585c15bf
[dyad] <jon.d@kassoehousing.com>
2025-10-13 15:06:41 +0100

    [dyad] wrote 1 file(s)

--- src/components/CountrySelector.tsx
+++ src/components/CountrySelector.tsx
@@ -40,13 +40,13 @@ const CountrySelector: React.FC<CountrySelectorProps> = ({ className }) => {
               </SelectTrigger>
               <SelectContent className="bg-popover text-popover-foreground">
                 {availableCountries.map((country) => (
-                  <SelectItem key={country.value} value={country.value} className="flex items-center gap-2 flex-nowrap"> {/* Added flex-nowrap here */}
+                  <SelectItem key={country.value} value={country.value} className="flex items-center gap-2 flex-nowrap min-w-max"> {/* Added min-w-max here */}
                     {country.value !== 'all' ? (
                       <CountryFlag countryName={country.value} className="flex-shrink-0" />
                     ) : (
                       <span className="text-lg flex-shrink-0">🌐</span>
                     )}
-                    <span className="whitespace-nowrap flex-shrink-0">{country.label}</span> {/* Added flex-shrink-0 here */}
+                    <span className="whitespace-nowrap flex-shrink-0">{country.label}</span>
                   </SelectItem>
                 ))}
               </SelectContent>
