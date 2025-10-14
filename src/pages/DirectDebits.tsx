80a378cd7b740d35135e2cec45cfa4e55766c46b
[dyad] <jon.d@kassoehousing.com>
2025-10-14 09:33:49 +0100

    [dyad] Made the "Add New Direct Debit" form scrollable - wrote 1 file(s)

--- src/pages/DirectDebits.tsx
+++ src/pages/DirectDebits.tsx
@@ -224,7 +224,7 @@ const DirectDebits = () => {
                   <PlusCircle className="mr-2 h-4 w-4" /> Add New Direct Debit
                 </Button>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+              <DialogContent className="sm:max-w-[425px] max-h-[90vh] overflow-y-auto"> {/* Added max-h and overflow-y-auto */}
                 <DialogHeader>
                   <DialogTitle>Add New Direct Debit</DialogTitle>
                 </DialogHeader>
