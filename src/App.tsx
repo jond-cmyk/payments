bce11331886670eb9b9fee17bbfe6e3d3afcab64
[dyad] <jon.d@kassoehousing.com>
2025-10-20 13:37:38 +0100

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,12 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter 
+            future={{ 
+              v7_startTransition: true, 
+              v7_relativeSplatPath: true 
+            }}
+          >
             <SessionContextProvider>
               <NotificationProvider>
                 <CountryProvider>
