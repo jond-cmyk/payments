a33fa1937bc0ba8724522ca16a220458d30efc5b
[dyad] <jon.d@kassoehousing.com>
2025-11-08 13:26:26 +0000

    [dyad] Reordered sidebar menu items. - wrote 1 file(s)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -136,8 +136,8 @@ const Sidebar = ({ isMobile = false }: SidebarProps) => { // Removed className f
           <div className="h-px bg-dyad-blue-foreground my-4" />
           <NavLink to="/admin/customers" icon={<Users className="h-5 w-5" />} label="Customers" />
           <NavLink to="/customer-deposit-returns" icon={<DollarSign className="h-5 w-5" />} label="Customer Deposit Returns" />
-          <NavLink to="/deposit-return-advisements" icon={<DollarSign className="h-5 w-5" />} label="Deposit Return Advisements" />
           <NavLink to="/landlord-deposits" icon={<HomeIcon className="h-5 w-5" />} label="Landlord Deposits" />
+          <NavLink to="/deposit-return-advisements" icon={<DollarSign className="h-5 w-5" />} label="Deposit Return Advisements" />
           
           <div className="h-px bg-dyad-blue-foreground my-4" /> 
 
