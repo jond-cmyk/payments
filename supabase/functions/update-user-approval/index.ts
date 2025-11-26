c759a7c6ffae7750cad12a25c0e8b57827fda696
[dyad] <jon.d@kassoehousing.com>
2025-11-26 12:36:34 +0000

    [dyad] Fix CORS crash by using valid Deno import (std@0.168.0) - wrote 4 file(s)

--- supabase/functions/update-user-approval/index.ts
+++ supabase/functions/update-user-approval/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 
